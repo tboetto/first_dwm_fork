ac6247f835d488f35abf80c524e2ca870bff17c6
Tony Boetto <tonyboetto@Tonys-MBP.lan>
2021-05-16 07:34:46 -0400

    patched bottomstack

--- config.def.h
+++ config.def.h
@@ -48,6 +48,8 @@ static const Layout layouts[] = {
  	{ "[\\]",     dwindle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
+ 	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
 };
 
 /* key definitions */
@@ -108,6 +110,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
  	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
  	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
+  { MODKEY,                       XK_b,      setlayout,      {.v = &layouts[7]} },
+ 	{ MODKEY|ShiftMask,             XK_b,      setlayout,      {.v = &layouts[8]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
